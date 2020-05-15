6f6d7f4270acb5c89d4e985da59aa8eb2375d0b9
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-15 15:03:35 -0400

    relocated the View Models, created SD utility, added references to projects

--- RuggedBooks/Controllers/HomeController.cs
+++ RuggedBooks/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using RuggedBooks.Models;
+using RuggedBooksModels.ViewModels;
 
 namespace RuggedBooks.Controllers
 {
