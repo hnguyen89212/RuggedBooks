cfe011ef7b22f3acfd291319421f881271e5c930
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-15 13:58:23 -0400

    fresh install

---
+++ RuggedBooks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RuggedBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
