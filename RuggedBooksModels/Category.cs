e6f2dbf83413af0a06c662de595235baaa458a5a
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-15 21:01:02 -0400

    created Category class, migrated

---
+++ RuggedBooksModels/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RuggedBooksModels
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public string CategoryName { get; set; }
+    }
+}
