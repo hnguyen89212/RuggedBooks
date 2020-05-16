aea8d456e74020533f8c3148c8d8197dcd9cfc53
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-16 16:05:50 -0400

    implemented unit of work

--- RuggedBooks/Startup.cs
+++ RuggedBooks/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RuggedBooksDAL.Data;
+using RuggedBooksDAL.Repository.IRepository;
 
 namespace RuggedBooks
 {
@@ -32,6 +33,7 @@ namespace RuggedBooks
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddScoped<IUnitOfWork, IUnitOfWork>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
         }
