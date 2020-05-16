16ee05426fad6f2ab9626f268b244202085a3414
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-16 16:05:50 -0400

    implemented unit of work

---
+++ RuggedBooksDAL/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuggedBooksDAL.Repository.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+
+        IStoredProcedureCall StoredProcedureCall { get; }
+    }
+}
---
+++ RuggedBooksDAL/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using RuggedBooksDAL.Data;
+using RuggedBooksDAL.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuggedBooksDAL.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+
+        public ICategoryRepository Category { get; private set; }
+
+        public IStoredProcedureCall StoredProcedureCall { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _db = context;
+            Category = new CategoryRepository(_db);
+            StoredProcedureCall = new StoredProcedureCall(_db);
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
