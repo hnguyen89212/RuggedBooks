1897172ec2bae7e4a6d81d35cedf111cf0ca5ae4
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-15 21:01:02 -0400

    created Category class, migrated

--- RuggedBooksDAL/Data/ApplicationDbContext.cs
+++ RuggedBooksDAL/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RuggedBooksModels;
 
 namespace RuggedBooksDAL.Data
 {
@@ -12,5 +13,8 @@ namespace RuggedBooksDAL.Data
             : base(options)
         {
         }
+
+        // Registers the Category.class
+        public DbSet<Category> Categories { get; set; }
     }
 }
