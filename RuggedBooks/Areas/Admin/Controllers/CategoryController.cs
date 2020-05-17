a6a2747bc6bdd88b5703dad4cec3367b2cf485db
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-16 21:21:12 -0400

    added region for API Calls, implemented GetAll

--- RuggedBooks/Areas/Admin/Controllers/CategoryController.cs
+++ RuggedBooks/Areas/Admin/Controllers/CategoryController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RuggedBooksDAL.Repository.IRepository;
 
 namespace RuggedBooks.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allCategories = _unitOfWork.Category.GetAll();
+            return Json(new {
+                data = allCategories
+            });
+        }
+
+        #endregion
     }
 }
