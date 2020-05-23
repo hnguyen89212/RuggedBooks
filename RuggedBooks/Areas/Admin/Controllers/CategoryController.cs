56dcf1cd6637c22048d323a2ebfa721ecc0e60f9
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-23 09:52:36 -0400

    added Upsert in CategoryController

--- RuggedBooks/Areas/Admin/Controllers/CategoryController.cs
+++ RuggedBooks/Areas/Admin/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RuggedBooksDAL.Repository.IRepository;
+using RuggedBooksModels;
 
 namespace RuggedBooks.Areas.Admin.Controllers
 {
@@ -20,6 +21,29 @@ namespace RuggedBooks.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Upsert(int? Id)
+        {
+            Category category = new Category();
+
+            // Id is null when we create/add a new cateogry
+            if (Id == null)
+            {
+                return View(Id);
+            }
+
+            // Otherwise, we are trying to update an existing? category
+            // For an integer, the default value is 0.
+            category = _unitOfWork.Category.Get(Id.GetValueOrDefault());
+
+            // The Id does not associate with any category.
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
         }
 
         #region API Calls
