c1fb7198e5048bf251836b7dea8e6076fb8c8775
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-23 19:13:35 -0400

    used Dapper and stored procedure for crud

--- RuggedBooks/Areas/Admin/Controllers/CoverTypeController.cs
+++ RuggedBooks/Areas/Admin/Controllers/CoverTypeController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using RuggedBooksDAL.Repository.IRepository;
 using RuggedBooksModels;
+using RuggedBooksUtilities;
 
 namespace RuggedBooks.Areas.Admin.Controllers
 {
@@ -32,7 +34,10 @@ namespace RuggedBooks.Areas.Admin.Controllers
                 return View(coverType);
             }
 
-            coverType = _unitOfWork.CoverType.Get(Id.GetValueOrDefault());
+            //coverType = _unitOfWork.CoverType.Get(Id.GetValueOrDefault());
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", Id);
+            coverType = _unitOfWork.StoredProcedureCall.OneRecord<CoverType>(SD.Procedure_CoverType_Get, parameter);
 
             if (coverType == null)
             {
@@ -49,16 +54,22 @@ namespace RuggedBooks.Areas.Admin.Controllers
             // Where is ModelState from? It is crafted automatically.
             if (ModelState.IsValid)
             {
+                var parameter = new DynamicParameters();
+                parameter.Add("@Name", coverType.Name);
+
                 if (coverType.Id == 0)
                 {
-                    _unitOfWork.CoverType.Add(coverType);
+                    //_unitOfWork.CoverType.Add(coverType);
+                    _unitOfWork.StoredProcedureCall.Execute(SD.Procedure_CoverType_Create, parameter);
                 }
                 else
                 {
-                    _unitOfWork.CoverType.Update(coverType);
+                    //_unitOfWork.CoverType.Update(coverType);
+                    parameter.Add("@Id", coverType.Id);
+                    _unitOfWork.StoredProcedureCall.Execute(SD.Procedure_CoverType_Update, parameter);
                 }
                 _unitOfWork.Save();
-                //return RedirectToAction("Index");
+
                 return RedirectToAction(nameof(Index));
             }
             return View(coverType);
@@ -69,7 +80,9 @@ namespace RuggedBooks.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allCovertypes = _unitOfWork.CoverType.GetAll();
+            //var allCovertypes = _unitOfWork.CoverType.GetAll();
+            var allCovertypes = _unitOfWork.StoredProcedureCall.List<CoverType>(SD.Procedure_CoverType_GetAll, null);
+
             return Json(new
             {
                 data = allCovertypes
@@ -79,15 +92,20 @@ namespace RuggedBooks.Areas.Admin.Controllers
         [HttpDelete]
         public IActionResult Delete(int Id)
         {
-            CoverType coverType = _unitOfWork.CoverType.Get(Id);
+            //CoverType coverType = _unitOfWork.CoverType.Get(Id
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", Id);
+            CoverType coverType = _unitOfWork.StoredProcedureCall.OneRecord<CoverType>(SD.Procedure_CoverType_Get, parameter);
 
             if (coverType == null)
             {
                 return Json(new { success = false, message = "Error removing cover type. Please try again." });
             }
 
-            _unitOfWork.CoverType.Remove(coverType);
+            //_unitOfWork.CoverType.Remove(coverType);
+            _unitOfWork.StoredProcedureCall.Execute(SD.Procedure_CoverType_Delete, parameter);
             _unitOfWork.Save();
+
             return Json(new { success = true, message = "Cover type is successfully removed." });
         }
 
