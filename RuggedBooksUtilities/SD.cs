f22d1fbfd0ea679d7cd57e63d0d27f7b14a92d6a
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-23 19:13:35 -0400

    used Dapper and stored procedure for crud

--- RuggedBooksUtilities/SD.cs
+++ RuggedBooksUtilities/SD.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace RuggedBooksUtilities
 {
-    public class SD
+    public static class SD
     {
+        public const string Procedure_CoverType_Create = "usp_CreateCoverType";
+        public const string Procedure_CoverType_Get = "usp_GetCoverType";
+        public const string Procedure_CoverType_GetAll = "usp_GetCoverTypes";
+        public const string Procedure_CoverType_Update = "usp_UpdateCoverType";
+        public const string Procedure_CoverType_Delete = "usp_DeleteCoverType";
     }
 }
