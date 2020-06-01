3faf2aff55e3bde193afe270e1767978d67f5f1c
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-06-01 09:53:59 -0400

    wired up EmailSender, CompanyList, Roles

--- RuggedBooks/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RuggedBooks/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,8 +11,12 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using RuggedBooksDAL.Repository.IRepository;
+using RuggedBooksModels;
+using RuggedBooksUtilities;
 
 namespace RuggedBooks.Areas.Identity.Pages.Account
 {
@@ -23,17 +27,25 @@ namespace RuggedBooks.Areas.Identity.Pages.Account
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        // an object that manages the roles in app.
+        // Goes to Startup.cs and makes: services.AddIdentity<IdentityUser, IdentityRole>
+        private readonly RoleManager<IdentityRole> _roleManager; 
+        private readonly IUnitOfWork _unitOfWork;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager,
+            IUnitOfWork unitOfWork)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
         }
 
         [BindProperty]
@@ -83,11 +95,29 @@ namespace RuggedBooks.Areas.Identity.Pages.Account
             public string Role { get; set; }
 
             public int? CompanyId { get; set; }
+
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
+
+            public IEnumerable<SelectListItem> RoleList { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
         {
             ReturnUrl = returnUrl;
+
+            // Initializes 2 lists for use in Register form.
+            // Only Admin user could see those lists.
+            Input = new InputModel() { 
+                CompanyList = _unitOfWork.Company.GetAll().Select(company => new SelectListItem { 
+                    Text = company.Name,
+                    Value = company.Id.ToString()
+                }),
+                RoleList = _roleManager.Roles.Where(role => role.Name != SD.Role_User_Individual).Select(role => role.Name).Select(role => new SelectListItem { 
+                    Text = role,
+                    Value = role
+                })
+            };
+
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
@@ -97,22 +127,67 @@ namespace RuggedBooks.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                //var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    CompanyId = Input.CompanyId,
+                    StreetAddress = Input.StreetAddress,
+                    City = Input.State,
+                    State = Input.State,
+                    PostalCode = Input.PostalCode,
+                    Name = Input.Name,
+                    PhoneNumber = Input.PhoneNumber,
+                    Role = Input.Role
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
+                    // We want to create all the roles right when initializing the DB.
+                    if (!await _roleManager.RoleExistsAsync(SD.Role_Administrator))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Role_Administrator));
+                    }
+                    if (!await _roleManager.RoleExistsAsync(SD.Role_Employee))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee));
+                    }
+                    if (!await _roleManager.RoleExistsAsync(SD.Role_User_Company))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Company));
+                    }
+                    if (!await _roleManager.RoleExistsAsync(SD.Role_User_Individual))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Individual));
+                    }
+                    // The 1st user would be superuser, the admin.
+                    //await _userManager.AddToRoleAsync(user, SD.Role_Administrator);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    if (user.Role == null)
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.Role_User_Individual);
+                    }
+                    else
+                    {
+                        if (user.CompanyId > 0)
+                        {
+                            await _userManager.AddToRoleAsync(user, SD.Role_User_Company);
+                        }
+                        await _userManager.AddToRoleAsync(user, user.Role);
+                    }
+
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                    //    protocol: Request.Scheme);
+
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
@@ -120,8 +195,19 @@ namespace RuggedBooks.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
+                        if (user.Role == null)
+                        {
+                            // User signs himself up.
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                            return LocalRedirect(returnUrl);
+                        }
+                        else
+                        {
+                            // An admin is registering/signing up a user.
+                            // Keeps the admin logged in and redirects him to a page where he could see a list of all users.
+                            // Index page of User controller in the Admin area.
+                            return RedirectToAction("Index", "User", new { Area = "Admin"});
+                        }
                     }
                 }
                 foreach (var error in result.Errors)
