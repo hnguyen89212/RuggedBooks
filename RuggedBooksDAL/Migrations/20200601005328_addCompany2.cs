08dac98821c45d9fa6d806a699ababc857fca90a
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-31 20:56:53 -0400

    added Company class, hooked it with ApplicationUser

---
+++ RuggedBooksDAL/Migrations/20200601005328_addCompany2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RuggedBooksDAL.Migrations
+{
+    public partial class addCompany2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    StreetAddress = table.Column<string>(nullable: false),
+                    City = table.Column<string>(nullable: false),
+                    PostalCode = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: false),
+                    IsAuthorizedCompany = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
