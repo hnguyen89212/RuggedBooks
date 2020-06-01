1f9077521dfca5c6a22957371d54e568d4af3a95
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-31 21:48:29 -0400

    hooked ApplicationUser to repository

---
+++ RuggedBooksDAL/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using RuggedBooksDAL.Data;
+using RuggedBooksDAL.Repository.IRepository;
+using RuggedBooksModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuggedBooksDAL.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext context) : base(context)
+        {
+            _db = context;
+        }
+
+    }
+}
---
+++ RuggedBooksDAL/Repository/IRepository/IApplicationUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using RuggedBooksModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuggedBooksDAL.Repository.IRepository
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+
+    }
+}
