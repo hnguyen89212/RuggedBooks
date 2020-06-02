cde686f2c1919ff9fadff2db9109e4f58d4188ee
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-06-02 09:12:15 -0400

    used SendGrid as Email sender, NOT done

--- RuggedBooksUtilities/TempEmailSender.cs
+++ RuggedBooksUtilities/TempEmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,9 +14,27 @@ namespace RuggedBooksUtilities
     // Configures the app to use this in Startup.cs
     public class TempEmailSender : IEmailSender
     {
+        private readonly EmailOptions emailOptions;
+
+        public TempEmailSender(IOptions<EmailOptions> emailOptions)
+        {
+            this.emailOptions = emailOptions.Value;
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            return Execute(emailOptions.SendGridKey, subject, htmlMessage, email);
+        }
+
+        public Task Execute(string sendGridKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(sendGridKey);
+            var from = new EmailAddress("phuchai1994@gmail.com", "Rugged Books Admins"); //any fake email
+            var to = new EmailAddress(email, "Hai Nguyen");
+            //var plainTextContent = "and easy to do anywhere, even with C#";
+            //var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", message);
+            return client.SendEmailAsync(msg);
         }
     }
 }
