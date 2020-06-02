a1a2392b3f1dfa033d86fb8343c5b8f33d579b6d
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-06-02 16:02:53 -0400

    finished AddToCart() in HomeController

--- RuggedBooks/Areas/Customer/Controllers/HomeController.cs
+++ RuggedBooks/Areas/Customer/Controllers/HomeController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RuggedBooksDAL.Repository;
@@ -27,7 +29,7 @@ namespace RuggedBooks.Area.Customer.Controllers
         // Retrieves all the products in db and render them in the homepage.
         public IActionResult Index()
         {
-            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return View(products);
         }
 
@@ -44,7 +46,7 @@ namespace RuggedBooks.Area.Customer.Controllers
 
         public IActionResult Details(int productId)
         {
-            var product = _unitOfWork.Product.GetFirstOrDefault(each => each.Id == productId, includeProperties:"Category,CoverType");
+            var product = _unitOfWork.Product.GetFirstOrDefault(each => each.Id == productId, includeProperties: "Category,CoverType");
 
             ShoppingCart shoppingCart = new ShoppingCart()
             {
@@ -53,6 +55,58 @@ namespace RuggedBooks.Area.Customer.Controllers
             };
 
             return View(shoppingCart);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            shoppingCart.Id = 0;
+
+            if (ModelState.IsValid)
+            {
+                // fetches the current user
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+                // Sets the cart owner
+                shoppingCart.ApplicationUserId = claim.Value;
+
+                //
+                ShoppingCart shoppingCartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(
+                    each => each.ApplicationUserId == shoppingCart.ApplicationUserId && each.ProductId == shoppingCart.ProductId
+                );
+
+                if (shoppingCartFromDb == null)
+                {
+                    // there is no record of this user buying this product.
+                    _unitOfWork.ShoppingCart.Add(shoppingCart);
+                }
+                else
+                {
+                    // updates
+                    shoppingCartFromDb.Count += shoppingCart.Count;
+                    _unitOfWork.ShoppingCart.Update(shoppingCartFromDb);
+                }
+                _unitOfWork.Save();
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                // We re-render the Details page with the same old product
+
+                var product = _unitOfWork.Product.GetFirstOrDefault(each => each.Id == shoppingCart.ProductId, includeProperties: "Category,CoverType");
+
+                shoppingCart = new ShoppingCart()
+                {
+                    Product = product,
+                    ProductId = product.Id
+                };
+
+                return View(shoppingCart);
+            }
         }
     }
 }
