23272eb009b80d3d45b519702f668541c5878489
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-06-03 21:59:01 -0400

    added SessionExtension and configured it in Startup

---
+++ RuggedBooksUtilities/SessionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuggedBooksUtilities
+{
+    public static class SessionExtension
+    {
+        public static void setObject(this ISession session, string key, Object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T getObject<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return (value == null) ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
