fddc2275b4d2fc632b85f2967d75178941a6f5a1
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-06-04 12:40:39 -0400

    added classes and interfaces for SMTP email client, finished sending emails

---
+++ RuggedBooksUtilities/EmailWithMailKit/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuggedBooksUtilities.EmailWithMailKit
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailMessage emailMessage);
+
+        List<EmailMessage> ReceiveEmail(int maxCount = 10);
+    }
+}
