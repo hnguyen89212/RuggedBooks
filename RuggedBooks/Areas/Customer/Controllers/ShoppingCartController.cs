09ee1f12e3e11bdf498d561a0cf95d470718fe5a
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-06-04 18:03:28 -0400

    added ShoppingCart Summary and view

--- RuggedBooks/Areas/Customer/Controllers/ShoppingCartController.cs
+++ RuggedBooks/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using RuggedBooksDAL.Repository.IRepository;
 using RuggedBooksModels;
 using RuggedBooksModels.ViewModels;
 using RuggedBooksUtilities;
+using RuggedBooksUtilities.EmailWithMailKit;
 
 namespace RuggedBooks.Areas.Customer.Controllers
 {
@@ -21,14 +25,17 @@ namespace RuggedBooks.Areas.Customer.Controllers
 
         private readonly IEmailSender _emailSender;
 
+        private readonly IEmailService _emailService;
+
         private readonly UserManager<IdentityUser> _userManager;
 
         public ShoppingCartVM ShoppingCartVM { get; set; }
 
-        public ShoppingCartController(IUnitOfWork unitOfWork, IEmailSender emailSender, UserManager<IdentityUser> userManager)
+        public ShoppingCartController(IUnitOfWork unitOfWork, IEmailSender emailSender, IEmailService emailService, UserManager<IdentityUser> userManager)
         {
             _unitOfWork = unitOfWork;
             _emailSender = emailSender;
+            _emailService = emailService;
             _userManager = userManager;
         }
 
@@ -63,6 +70,38 @@ namespace RuggedBooks.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        [HttpPost]
+        [ActionName("Index")]
+        public async Task<IActionResult> IndexPOST()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var user = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claim.Value);
+
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Verification email is empty!");
+            }
+
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { area = "Identity", userId = user.Id, code = code },
+                protocol: Request.Scheme);
+
+            EmailMessage emailMessage = new EmailMessage();
+            EmailAddress to = new EmailAddress("New Valued Customer", user.Email);
+            emailMessage.ToAddresses.Add(to);
+            emailMessage.Subject = "Confirm Your Email at RuggedBooks";
+            emailMessage.Content = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.";
+            _emailService.SendEmail(emailMessage);
+
+            ModelState.AddModelError(string.Empty, "Verification email sent. Please check your inbox.");
+            return RedirectToAction("Index");
+        }
+
         public IActionResult IncrementQuantity(int cartId)
         {
             var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId, includeProperties: "Product");
@@ -91,7 +130,7 @@ namespace RuggedBooks.Areas.Customer.Controllers
                 _unitOfWork.ShoppingCart.Remove(cart);
 
                 _unitOfWork.Save();
-                
+
                 HttpContext.Session.SetInt32(SD.Shopping_Cart_Session, totalItemsInCart - 1);
             }
             else
@@ -116,12 +155,42 @@ namespace RuggedBooks.Areas.Customer.Controllers
                 .Count;
 
             _unitOfWork.ShoppingCart.Remove(cart);
-            
+
             _unitOfWork.Save();
-            
+
             HttpContext.Session.SetInt32(SD.Shopping_Cart_Session, --totalItemsInCart);
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                OrderHeader = new OrderHeader(),
+                ShoppingCarts = _unitOfWork.ShoppingCart
+                    .GetAll(c => c.ApplicationUserId == claim.Value, includeProperties: "Product")
+            };
+
+            ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser
+                .GetFirstOrDefault(c => c.Id == claim.Value, includeProperties: "Company");
+
+            foreach (var cart in ShoppingCartVM.ShoppingCarts)
+            {
+                cart.Price = Utilities.GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+                ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            }
+            ShoppingCartVM.OrderHeader.Name = ShoppingCartVM.OrderHeader.ApplicationUser.Name;
+            ShoppingCartVM.OrderHeader.PhoneNumber = ShoppingCartVM.OrderHeader.ApplicationUser.PhoneNumber;
+            ShoppingCartVM.OrderHeader.StreetAddress = ShoppingCartVM.OrderHeader.ApplicationUser.StreetAddress;
+            ShoppingCartVM.OrderHeader.City = ShoppingCartVM.OrderHeader.ApplicationUser.City;
+            ShoppingCartVM.OrderHeader.State = ShoppingCartVM.OrderHeader.ApplicationUser.State;
+            ShoppingCartVM.OrderHeader.PostalCode = ShoppingCartVM.OrderHeader.ApplicationUser.PostalCode;
+
+            return View(ShoppingCartVM);
         }
     }
 }
