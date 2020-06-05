60c27981fd1a060aa0124a6b77ec6e013c5b1b0d
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-06-05 16:10:39 -0400

    completed Order Index and Order controller

--- RuggedBooks/Areas/Admin/Controllers/OrderController.cs
+++ RuggedBooks/Areas/Admin/Controllers/OrderController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RuggedBooksDAL.Repository.IRepository;
 using RuggedBooksModels;
+using RuggedBooksUtilities;
 using Stripe;
 
 namespace RuggedBooks.Areas.Admin.Controllers
@@ -29,13 +31,50 @@ namespace RuggedBooks.Areas.Admin.Controllers
         #region API Calls
 
         [HttpGet]
-        public IActionResult GetOrderList()
+        public IActionResult GetOrderList(string status)
         {
-            IEnumerable<OrderHeader> orderHeaders;
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
-            orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties:"ApplicationUser");
+            IEnumerable<OrderHeader> orderHeaderList;
 
-            return Json(new { data = orderHeaders });
+            // If the user is an admin or an employee, then we load all the orders.
+            if (User.IsInRole(SD.Role_Administrator) || User.IsInRole(SD.Role_Employee))
+            {
+                orderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            // Otherwise, we just show them their own orders.
+            else
+            {
+                orderHeaderList = _unitOfWork.OrderHeader.GetAll(
+                                        u => u.ApplicationUserId == claim.Value,
+                                        includeProperties: "ApplicationUser");
+            }
+
+            // Based on the status, we filter out the list once more.
+            switch (status)
+            {
+                case "pending":
+                    orderHeaderList = orderHeaderList.Where(o => o.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusApproved ||
+                                                            o.OrderStatus == SD.StatusInProcess ||
+                                                            o.OrderStatus == SD.StatusPending);
+                    break;
+                case "completed":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusShipped);
+                    break;
+                case "rejected":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusCancelled ||
+                                                            o.OrderStatus == SD.StatusRefunded ||
+                                                            o.OrderStatus == SD.PaymentStatusRejected);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaderList });
         }
 
         #endregion
