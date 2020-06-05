65ae378e6be236054703dcecc45963d3d2e02002
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-06-05 15:51:20 -0400

    added OrderController

---
+++ RuggedBooks/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RuggedBooksDAL.Repository.IRepository;
+using RuggedBooksModels;
+using Stripe;
+
+namespace RuggedBooks.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetOrderList()
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+
+            orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties:"ApplicationUser");
+
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+    }
+}
